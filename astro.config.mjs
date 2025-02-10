a70025f2215939cda1c6856a4e68563804c9cf9f
Roman Golev <67314258+romangolev@users.noreply.github.com>
2025-02-10 23:20:57 +0100

    fix: update astro and pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.romangolev.com',
+  base: '/astro_portfolio/',
+  // site: 'https://www.romangolev.com',
   integrations: [],
 
   vite: {
